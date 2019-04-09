9bb5f7c0c62b97b3fec41b460e9b7e738f3a43da
Marco Smit (FG-AM-6) <Marco.Smit@bmwgroup.com>
2019-04-09 14:07:34 -0400

    link to higher ref eks module --> 2.0.0

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "vpc" {
 
 # Kubernetes cluster:
 module "kubernetes_cluster_application" {
-  source = "github.com/kentrikos/terraform-aws-eks?ref=0.2.2"
+  source = "github.com/kentrikos/terraform-aws-eks?ref=2.0.0"
 
   cluster_prefix                = "${local.cluster_name}"
   region                        = "${var.region}"
