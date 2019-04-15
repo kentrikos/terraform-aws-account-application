d9199f36a84edf7ce0bf2da408868866f6d44792
Michael Greer <michael.greer@bmwgroup.com>
2019-04-15 13:57:05 -0400

    added cluster outputs

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ module "vpc" {
 
 # Kubernetes cluster:
 module "kubernetes_cluster_application" {
-  source = "github.com/kentrikos/terraform-aws-eks?ref=0.2.2"
+  source = "github.com/kentrikos/terraform-aws-eks?ref=1.1.0"
 
   cluster_prefix                = "${local.cluster_name}"
   region                        = "${var.region}"
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,7 @@
+output "cluster_id" {
+  value = "${module.kubernetes_cluster_application.cluster_id}"
+}
 
+output "worker_iam_role_name" {
+  value = "${module.kubernetes_cluster_application.worker_iam_role_name}"
+}
